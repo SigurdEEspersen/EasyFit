29c4f1fcdb2995063a106319a539f87af58f3f5a
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2021-03-20 17:22:16 +0100

    added cardview

--- app/src/main/java/eugen/enterprise/easyfit/view/adapters/WorkoutAdapter.java
+++ app/src/main/java/eugen/enterprise/easyfit/view/adapters/WorkoutAdapter.java
@@ -5,10 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.transition.AutoTransition;
+import androidx.transition.TransitionManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +31,11 @@ public class WorkoutAdapter extends ArrayAdapter<IMuscleGroup> {
 
     static class ViewHolder {
         TextView txt_workout_muscleGroup;
+        ImageButton img_expander;
+        LinearLayout btn_expandExercises;
+        RelativeLayout layout_exercises;
+        CardView cardView;
+        TextView test;
     }
 
     @Override
@@ -49,12 +60,37 @@ public class WorkoutAdapter extends ArrayAdapter<IMuscleGroup> {
             row = inflater.inflate(R.layout.workout_card, parent, false);
             viewHolder = new ViewHolder();
             viewHolder.txt_workout_muscleGroup = row.findViewById(R.id.txt_workout_muscleGroup);
+            viewHolder.cardView = row.findViewById(R.id.workout_baseCard);
+            viewHolder.img_expander = row.findViewById(R.id.img_expander);
+            viewHolder.layout_exercises = row.findViewById(R.id.layout_exercises);
+            viewHolder.btn_expandExercises = row.findViewById(R.id.btn_expandExercises);
+            viewHolder.test = row.findViewById(R.id.txt_test);
             row.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) row.getTag();
         }
         final IMuscleGroup muscleGroup = getItem(position);
         viewHolder.txt_workout_muscleGroup.setText(muscleGroup.getName());
+
+        viewHolder.btn_expandExercises.setOnClickListener(v -> {
+            if (viewHolder.layout_exercises.getVisibility() == View.GONE) {
+                TransitionManager.beginDelayedTransition(viewHolder.cardView,
+                        new AutoTransition());
+                viewHolder.layout_exercises.setVisibility(View.VISIBLE);
+                viewHolder.img_expander.setImageResource(R.drawable.ic_baseline_expand_less_24);
+            } else if (viewHolder.layout_exercises.getVisibility() == View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(viewHolder.cardView,
+                        new AutoTransition());
+                viewHolder.layout_exercises.setVisibility(View.GONE);
+                viewHolder.img_expander.setImageResource(R.drawable.ic_baseline_expand_more_24);
+            }
+        });
+
+        String testString = "";
+        for (int i = 0; i < muscleGroup.getExercises().size(); i++) {
+            testString += muscleGroup.getExercises().get(i).getName() + "\n";
+        }
+        viewHolder.test.setText(testString);
 
         return row;
     }
