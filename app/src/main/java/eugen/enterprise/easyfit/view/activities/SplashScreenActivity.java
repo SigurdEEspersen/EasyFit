06b58621aab2c88ec8f22bc6471ed074e50b3811
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2021-05-13 12:38:18 +0200

    logo and loading screen

---
+++ app/src/main/java/eugen/enterprise/easyfit/view/activities/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package eugen.enterprise.easyfit.view.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import eugen.enterprise.easyfit.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, 750);
+    }
+}
